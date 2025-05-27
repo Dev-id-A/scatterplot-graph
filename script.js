7d3390874be9ce647d372ab83b3077bf7b05d089
Dev-id-A <davidaranzana618@gmail.com>
2025-05-27 13:16:38 +0200

    axis created

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//Variables
+    const width = 800;
+    const height = 500;
+    const padding = 150;
+
 //Fetching data  
     async function fetchData() {
         try{
@@ -12,21 +17,53 @@
     }
 
 //SVG creation
-    const createSVG = (data) =>{
-        const svg = d3.select("body")
-                        .append("svg")
-                        .attr("width", "100%")
-                        .attr("height", "100%")
-                        .attr("viewBox", "0 0 100 100")
-                        .attr("preserveAspectRatio", "xMidYMid meet");
+const createSVG = (data) => {
+    const svg = d3.select("body")
+        .append("svg")
+        .attr("width", "100%")
+        .attr("height", "100%")
+        .attr("viewBox", `0 0 ${width} ${height}`)
+        .attr("preserveAspectRatio", "xMidYMid meet")
+        .style("background-color", "wheat");
 
 //Title
         svg.append("text")
+            .attr("id", "title")
             .text("Most fastest riders and doping")
-            .attr("y", 5)
-            .attr("x", 33)
-            .attr("font-size", 3);
-            
+            .attr("y", 50)
+            .attr("x", 300)
+            .attr("font-size", 20);
+
+//xScale
+const minYear = d3.min(data,d => d.Year);
+const maxYear = d3.max(data,d => d.Year);
+
+        const xScale = d3.scaleLinear()
+                            .domain([minYear -1, maxYear +1])
+                            .range([padding, width - padding]);
+
+        const xAxis = d3.axisBottom(xScale)
+                            .tickFormat(d3.format("d"));
+        
+        svg.append("g")
+            .attr("id", "x-axis")
+            .attr("transform", `translate(0, ${height - padding})`)
+            .call(xAxis)
+
+//yScale
+const timeParse = d3.timeParse("%M:%S")
+
+        const yScale = d3.scaleTime()
+                            .domain(d3.extent(data, d => timeParse(d.Time)))
+                            .range([padding/2, height - padding]);
+
+        const yAxis = d3.axisLeft(yScale)
+                        .tickFormat(d3.timeFormat("%M:%S"));
+
+        svg.append("g")
+            .attr("id", "y-axis")
+            .attr("transform", `translate(${padding}, 0)`)
+            .call(yAxis)
     }
 
     fetchData()
